57c35f82010e5b61f205783f3f87ffb654e8a358
Stepan Shcherbakov <shc-stepan@yandex.ru>
2021-01-19 19:13:46 +0300

    Сделал сериализацию/десериализацию через файл

--- src/main/java/TemplateSerializer.java
+++ src/main/java/TemplateSerializer.java
@@ -1,9 +1,18 @@
+import com.sun.org.slf4j.internal.Logger;
+import com.sun.org.slf4j.internal.LoggerFactory;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class TemplateSerializer {
+    private final static Logger LOGGER = LoggerFactory.getLogger(TemplateSerializer.class);
+
     private final static Character DELIMITER = '#';
     private final static String FORMATTER = "%s";
     private final List<Pattern> patterns;
@@ -40,29 +49,53 @@ public class TemplateSerializer {
         this.hasValueAtEnd = hasValueAtEnd;
     }
 
-    public List<String> serialize(String text){
+    private List<String> serializeLine(String line){
         List<String> result = new ArrayList<>();
 
         Map<Pattern, Integer> indexCache = new HashMap<>(patterns.size());
 
-        patterns.forEach(pattern -> indexCache.put(pattern, KMP.search(pattern, text)));
+        patterns.forEach(pattern -> indexCache.put(pattern, KMP.search(pattern, line)));
 
         if (hasValueAtStart)
-            result.add(text.substring(0, indexCache.get(patterns.get(0))));
+            result.add(line.substring(0, indexCache.get(patterns.get(0))));
 
         for (int i = 0; i < patterns.size() - 1; i++) {
             Pattern leftPattern = patterns.get(i);
             Pattern rightPattern = patterns.get(i+1);
-            result.add(text.substring(indexCache.get(leftPattern) + leftPattern.getLength(), indexCache.get(rightPattern)));
+            result.add(line.substring(indexCache.get(leftPattern) + leftPattern.getLength(), indexCache.get(rightPattern)));
         }
 
         if (hasValueAtEnd)
-            result.add(text.substring(indexCache.get(patterns.get(patterns.size() - 1)) + patterns.get(patterns.size() - 1).getLength()));
+            result.add(line.substring(indexCache.get(patterns.get(patterns.size() - 1)) + patterns.get(patterns.size() - 1).getLength()));
 
         return result;
     }
 
-    public String deserialize(List<String> values){
+    private String deserializeLine(List<String> values){
         return String.format(format, values.toArray());
     }
+
+    public List<List<String>> serialize(Path pathToInputFile){
+        List<List<String>> result = new ArrayList<>();
+
+        try {
+            List<String> lines = Files.readAllLines(pathToInputFile);
+            lines.forEach(line -> result.add(serializeLine(line)));
+        } catch (IOException e) {
+            LOGGER.warn("Failed to serialize file:  " + pathToInputFile, e);
+        }
+
+        return result;
+    }
+
+    public void deserialize(List<List<String>> lineValues, Path pathToOutputFile){
+        List<String> lines = lineValues.stream().map(this::deserializeLine).collect(Collectors.toList());
+        try {
+            Files.write(pathToOutputFile, lines);
+        } catch (IOException e) {
+            LOGGER.warn("Failed to deserialize lines: " + lineValues, e);
+        }
+    }
+
+
 }
--- src/test/java/TemplateSerializerTest.java
+++ src/test/java/TemplateSerializerTest.java
@@ -1,8 +1,14 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -12,26 +18,21 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
  */
 public class TemplateSerializerTest {
 
-    @DisplayName("Тест паттерна такси")
+    @DisplayName("Тест сериализации из файла и десериализации в файл")
     @Test
-    void test_taxi_pattern(){
-        TemplateSerializer templateSerializer = new TemplateSerializer("В модуле # произошла ошибка # водителя #");
-        List<String> texts = Arrays.asList(
-                "В модуле такси произошла ошибка доступа водителя Ивана",
-                "В модуле доставки произошла ошибка подключения водителя Степана");
-        testPattern(templateSerializer, texts);
+    void test_file_serialization(){
+        TemplateSerializer taxiTemplateSerializer = new TemplateSerializer("В модуле # произошла ошибка # водителя #");
+        testFromFile(taxiTemplateSerializer, "taxi.example");
     }
 
     @DisplayName("Тест специфичных паттернов")
     @Test
     void test_specific_patterns(){
         TemplateSerializer chatTemplateSerializer = new TemplateSerializer("# зашел в чат");
-        List<String> chatTexts = Arrays.asList("Иван зашел в чат", "Степан зашел в чат");
-        testPattern(chatTemplateSerializer, chatTexts);
+        testFromFile(chatTemplateSerializer, "chat.example");
 
         TemplateSerializer happyBirthdayTemplateSerializer = new TemplateSerializer("С Днем Рождения, #");
-        List<String> hbTexts = Arrays.asList("С Днем Рождения, Иван", "С Днем Рождения, Степан");
-        testPattern(happyBirthdayTemplateSerializer, hbTexts);
+        testFromFile(happyBirthdayTemplateSerializer, "hb.example");
     }
 
     @DisplayName("Тест неверно заданного шаблона")
@@ -41,7 +42,24 @@ public class TemplateSerializerTest {
         assertThrows(IllegalArgumentException.class, () -> new TemplateSerializer(""));
     }
 
-    private static void testPattern(TemplateSerializer templateSerializer, List<String> texts){
-        texts.forEach(text -> assertEquals(text, templateSerializer.deserialize(templateSerializer.serialize(text))));
+    /**
+     * Вспомогательный класс для тестирования сериализации из файла и десериализации во временный файл
+     * @param templateSerializer шаблон для сериализации
+     * @param inputFileName имя файла с данными из ресурсов
+     */
+    private static void testFromFile(TemplateSerializer templateSerializer, String inputFileName){
+        try {
+            Path input = Paths.get(TemplateSerializerTest.class.getResource(inputFileName).toURI());
+            List<List<String>> serialized = templateSerializer.serialize(input);
+
+            Path tmp = Files.createTempFile("test"+ UUID.randomUUID(), ".example");
+            templateSerializer.deserialize(serialized, tmp);
+
+            assertEquals(Files.readAllLines(input), Files.readAllLines(tmp));
+
+            Files.delete(tmp);
+        } catch (URISyntaxException | IOException e) {
+            Assertions.fail("Тест провалился из-за проблем с файлами", e);
+        }
     }
 }
